8ab924d06a5576b1ddae66064da5ec74c96e66cf
Craig Wayne <craigiswayne@gmail.com>
2025-06-19 12:45:56 +0100

    v2.0.0

--- shell.js
+++ shell.js
@@ -1,33 +1,44 @@
-var toggle = function(t){
-    const parent = t.parentNode;
-    if(parent.dataset.callback && window[parent.dataset.callback]){
-        window[parent.dataset.callback]();
-    } else {
-        console.log('callback cant be called');
+const GameShell = {
+    toggle: (t) => {
+        const parent = t.parentNode;
+        const callback_name = parent.dataset.callback;
+
+        if (callback_name && typeof GameShell[callback_name] === 'function') {
+            GameShell[callback_name]();
+        } else {
+            console.log('callback cant be called');
+        }
+    },
+    do_action: (action_name) => {
+        console.log('doing action', action_name)
+    },
+    toggle_menu: () => {
+        document.querySelector('.sub-menu').classList.toggle('open')
+    },
+    toggle_fullscreen: () => {
+        if (document.fullscreenElement === null) {
+            const element = document.documentElement;
+            if (element.requestFullscreen) {
+                element.requestFullscreen();
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
+        }
+    },
+    init: () => {
+        document
+            .querySelectorAll('label.toggle input[type=checkbox]')
+            .forEach(i => {
+                i.addEventListener('change', (event) =>  GameShell.toggle(i));
+            })
+        document
+            .querySelectorAll('button.action')
+            .forEach(i => {
+                i.addEventListener('click', (event) =>  GameShell.do_action(i.dataset.action));
+            })
     }
 }
 
-var toggle_menu = function() {
-    document.querySelector('.sub-menu').classList.toggle('open')
-}
-
-var toggle_fullscreen = function() {
-    if (document.fullscreenElement === null) {
-        const element = document.documentElement;
-        if (element.requestFullscreen) {
-            element.requestFullscreen();
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        }
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    document
-        .querySelectorAll('.toggle input[type=checkbox]')
-        .forEach(i => {
-            i.addEventListener('change', (event) =>  toggle(i));
-        })
-})
+document.addEventListener('DOMContentLoaded', GameShell.init)
